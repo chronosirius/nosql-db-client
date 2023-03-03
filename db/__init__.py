a0075cbb38527127e18fb5777986aa19bae36f22
chronosirius <73508925+chronosirius@users.noreply.github.com>
2023-03-02 19:45:07 -0800

    Create __init__.py

---
+++ db/__init__.py
@@ -0,0 +1,57 @@
+import json
+from abc import ABC
+from os import listdir, remove, makedirs
+from threading import Lock
+from .observed_dict import WatchedDict
+from .observed_list import WatchedList
+
+class Database(ABC):
+	def __init__(self, dir):
+		self.dir = dir
+		try:
+			makedirs(self.dir)
+			print(f"[INFO] That directory ({self.dir}) didn't exist; it is created now.")
+		except FileExistsError:
+			pass
+		self.lock = Lock()
+
+	def __setitem__(self, key, value):
+		try:
+			towrite = json.dumps(value.jsonify(), indent=4)
+		except AttributeError:
+			towrite = json.dumps(value, indent=4)
+
+		self.lock.acquire()
+		with open(f'{self.dir}/{key}', 'w+') as f:
+			f.write(towrite)
+		self.lock.release()
+
+	def __getitem__(self, key):
+		if key in self.keys():
+			self.lock.acquire()
+			with open(f'{self.dir}/{key}', 'r') as f:
+				val = json.loads(f.read())
+			self.lock.release()
+			if type(val) is list:
+				return WatchedList(val, self, key)
+			elif type(val) is dict:
+				return WatchedDict(val, self, key)
+			else:
+				return val
+		else:
+			raise KeyError(key)
+
+	def __delitem__(self, key):
+		if key in self.keys():
+			remove(f'{self.dir}/{key}')
+		else:
+			raise KeyError(key)
+
+	def keys(self):
+		return set(listdir(f'{self.dir}/'))
+
+	def values(self):
+		return [self[key] for key in self.keys()]
+	
+	def __repr__(self):
+		return '<Database ('+self.dir+')>'
